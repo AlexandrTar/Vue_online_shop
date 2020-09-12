9920fb0d97fbc0059efab4dbde8d6c74eab1cc76
Alex Tarasov <bezzyck@yandex.ru>
2020-09-12 17:21:49 +0300

    init

---
+++ src/components/v-cart-item.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="v-cart-item">
+    <img class="v-cart-item__image"
+         :src=" require('../assets/images/' + cart_item_data.image)"
+         alt="img"
+    >
+    <div class="v-cart-item__info">
+      <p>Name: <span class="inf">{{cart_item_data.name}}</span></p>
+      <p>Price: <span class="inf">{{cart_item_data.price}}</span></p>
+      <p>Article: <span class="inf">{{cart_item_data.article}}</span></p>
+    </div>
+
+    <div class="v-cart-item__quantity">
+
+      <div class="quantity__title">Q-ty:</div>
+
+      <div class="quantity__content">
+        <span class="quantity__btn" @click="decrementItem">-</span>
+        {{cart_item_data.quantity}}
+        <span class="quantity__btn" @click="incrementItem">+</span>
+      </div>
+    </div>
+    <div class="v-cart-item__btn">
+      <button class="btn" @click="deleteFromCart">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-cart-item.vue",
+  props: {
+    cart_item_data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  mounted() {
+    this.$set(this.cart_item_data, 'quantity', 1)
+  },
+  methods: {
+    deleteFromCart() {
+      this.$emit('delete-from-cart')
+    },
+    decrementItem() {
+      this.$emit('decrement')
+    },
+    incrementItem() {
+      this.$emit('increment')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.v-cart-item{
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 8px 0 #e0e0e0;
+  padding: $padding*2;
+  margin-bottom: $margin*2;
+
+  min-width: 400px;
+  &__image{
+    height: 100px;
+   }
+
+}
+
+.quantity__title{
+  margin-bottom: 15px;
+}
+.quantity__btn {
+  cursor: pointer;
+  font-weight: 700;
+  padding: 10px;
+
+}
+.inf {
+  font-weight: 700;
+}
+</style>
---
+++ src/components/v-cart.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="v-cart">
+    <router-link :to="{name: 'catalog'}">
+      <div class="v-catalog__link_to_cart">
+        Back to catalog
+      </div>
+    </router-link>
+    <h2>Cart</h2>
+    <p v-if="!cart_data.length">There are no products in cart...</p>
+    <v-cart-item
+      v-for="(item, index) in cart_data"
+      :key="item.article"
+      :cart_item_data="item"
+      @delete-from-cart="deleteFromCart(index)"
+      @increment="increment(index)"
+      @decrement="decrement(index)"
+    />
+    <div class="v-cart__total">
+      <p class="total_name">Total:</p>
+      <p>{{cartTotalCost}} P.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import vCartItem from './v-cart-item'
+import {mapActions} from 'vuex'
+
+export default {
+  name: "v-cart",
+  components: {
+    vCartItem
+  },
+  props: {
+    cart_data: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  computed: {
+    cartTotalCost() {
+      return this.cart_data.reduce((res, item) => res + item.price * item.quantity, 0)
+    }
+  },
+  methods: {
+    ...mapActions([
+        'DELETE_FROM_CART',
+      'INCREMENT_CART_ITEM',
+      'DECREMENT_CART_ITEM'
+    ]),
+
+    deleteFromCart(index) {
+      this.DELETE_FROM_CART(index)
+    },
+
+    increment(index) {
+      this.INCREMENT_CART_ITEM(index)
+    },
+    decrement(index) {
+      this.DECREMENT_CART_ITEM(index)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.v-cart{
+  margin-bottom: 100px;
+  &__total{
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    padding: $padding*3;
+    display: flex;
+    justify-content: center;
+    box-shadow: 0 0 16px 0 #e0e0e0;
+    background: white;
+    font-weight: 700;
+    font-size: 20px;
+  }
+}
+.total_name{
+  margin-right: $margin*2;
+}
+</style>
